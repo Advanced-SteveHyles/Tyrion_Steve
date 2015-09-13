32db7c9553e2b4c51fce0a9bfb71fb4ec91135a7
Advanced-SteveHyles <Steven.HYles@hotmail.co.uk>
2015-09-13 09:41:49 +0100

    Added Tests for all 2s and all 3s.  Refactored to extract setup

--- KATAS/ClassLibrary1/UserStory1 .cs
+++ KATAS/ClassLibrary1/UserStory1 .cs
@@ -8,12 +8,15 @@ using Xunit;
 
 namespace ClassLibrary1
 {
-    public class OCR
+    public class UserStory1
     {
         private const string url = @"http://codingdojo.org/cgi-bin/index.pl?KataBankOCR";
 
         private readonly string[] _scenario1 = { " _  _  _  _  _  _  _  _  _ ", "| || || || || || || || || |", "|_||_||_||_||_||_||_||_||_|", "                           " };
         private readonly string[] _scenario2 = { "                           ", "  |  |  |  |  |  |  |  |  |", "  |  |  |  |  |  |  |  |  |", "                           " };
+        private readonly string[] _scenario3 = { " _  _  _  _  _  _  _  _  _ ", " _| _| _| _| _| _| _| _| _|", "|_ |_ |_ |_ |_ |_ |_ |_ |_ ", "                           " };
+        private readonly string[] _scenario4 = { " _  _  _  _  _  _  _  _  _ ", " _| _| _| _| _| _| _| _| _|", " _| _| _| _| _| _| _| _| _|", "                           " };
+
         /*
           " _  _  _  _  _  _  _  _  _ ", 
           "| || || || || || || || || |", 
@@ -58,7 +61,7 @@ namespace ClassLibrary1
 
 
         [Fact]
-        public void TestCase1_AccountNumberIsAllZeros()
+        public void Scenario1_AccountNumberIsAllZeros()
         {
             var lineParser = new LineParser(_scenario1);
             lineParser
@@ -67,6 +70,7 @@ namespace ClassLibrary1
 
             Assert.Equal("000000000", lineParser.AccountNumber);
         }
+
 
         [Fact]
         public void TestCase1_AccountNumberIsAllOnes()
@@ -77,6 +81,28 @@ namespace ClassLibrary1
                 .Parse();
 
             Assert.Equal("111111111", lineParser.AccountNumber);
+        }
+
+        [Fact]
+        public void Scenario3_AccountNumberIsAllTwos()
+        {
+            var lineParser = new LineParser(_scenario3);
+            lineParser
+                .Validate()
+                .Parse();
+
+            Assert.Equal("222222222", lineParser.AccountNumber);
+        }
+
+        [Fact]
+        public void Scenario4_AccountNumberIsAllThrees()
+        {
+            var lineParser = new LineParser(_scenario4);
+            lineParser
+                .Validate()
+                .Parse();
+
+            Assert.Equal("333333333", lineParser.AccountNumber);
         }
 
 
@@ -96,14 +122,29 @@ namespace ClassLibrary1
 
         public LineParser(string[] scenario1)
         {
-            _ocrMapping = new Dictionary<string, int>();
-            _ocrMapping.Add(string.Format("{0}{1}{2}{3}{4}{5}{6}{7}{8}", space, dash, space, pipe, space, pipe, pipe, dash, pipe), 0);
-            _ocrMapping.Add(string.Format("{0}{1}{2}{3}{4}{5}{6}{7}{8}", space, space, space, space, space, pipe, space, space, pipe), 1);
+            SetUpDictionary();
 
 
             _linesToParse = scenario1;
             _accountNumber = new StringBuilder();
         }
+
+        private void SetUpDictionary()
+        {
+            _ocrMapping = new Dictionary<string, int>();
+            _ocrMapping.Add(SetupMatrix(space, dash, space, pipe, space, pipe, pipe, dash, pipe, space, space, space), 0);
+            _ocrMapping.Add(SetupMatrix( space, space, space, space, space, pipe, space, space,pipe, space, space, space), 1);
+            _ocrMapping.Add(SetupMatrix(space, dash, space, space, dash, pipe, pipe, dash, space, space, space, space), 2);
+            _ocrMapping.Add(SetupMatrix(space, dash, space, space, dash, pipe, space, dash, pipe, space, space, space), 3);
+
+        }
+
+        private string SetupMatrix(string char1, string char2, string char3, string char4, string char5, string char6, string char7, string char8, string char9, string char10, string char11, string char12)
+        {
+            return string.Format("{0}{1}{2}{3}{4}{5}{6}{7}{8}{9}{10}{11}", char1, char2, char3, char4, char5, char6, char7, char8, char9, char10, char11, char12);
+        }
+
+
 
         public string AccountNumber
         {
@@ -137,18 +178,20 @@ namespace ClassLibrary1
             for (var index = 0; index < 27; index += 3)
             {
 
-                var testString = 
-                    string.Format("{0}{1}{2}{3}{4}{5}{6}{7}{8}",
+                var testString =
+                    string.Format("{0}{1}{2}{3}{4}{5}{6}{7}{8}{9}{10}{11}",
                     _linesToParse[0][index], _linesToParse[0][index + 1], _linesToParse[0][index + 2],
-                    _linesToParse[1][index] , _linesToParse[1][index + 1] ,_linesToParse[1][index + 2] ,
-                    _linesToParse[2][index] , _linesToParse[2][index + 1], _linesToParse[2][index + 2])
+                    _linesToParse[1][index], _linesToParse[1][index + 1], _linesToParse[1][index + 2],
+                    _linesToParse[2][index], _linesToParse[2][index + 1], _linesToParse[2][index + 2],
+                    _linesToParse[3][index], _linesToParse[3][index + 1], _linesToParse[3][index + 2])
                 ;
 
-
-                _accountNumber.Append(_ocrMapping[testString]);
+                    _accountNumber.Append(_ocrMapping[testString]);
+      
                 
+
             }
-            
+
             return this;
         }
 
