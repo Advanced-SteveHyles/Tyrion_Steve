d2d16b5b81aaf7f66bbdc27d382b5d2f7620baf8
Steven Hyles <steven.hyles@advancedcomputersoftware.com>
2015-09-16 09:44:14 +0100

    Added StevesFalse to chapter on PatternMatching

--- f#/FSHarp/File1.fs
+++ f#/FSHarp/File1.fs
@@ -4,17 +4,21 @@
 
 type Proposition = 
                 | True 
+                | False of Proposition
                 | Not of Proposition 
                 | And of Proposition * Proposition 
                 | Or of Proposition * Proposition
                 | Xor of Proposition * Proposition
+                | And3 of Proposition * Proposition * Proposition 
 
 let rec eval x = match x with 
                | True -> true    
+               | False ->  (eval not True)               
                | Not (prop) -> not (eval prop)     
                | And (prop1, prop2) -> (eval prop1) && (eval prop2)     
                | Or (prop1, prop2) -> (eval prop1) || (eval prop2)
                | Xor (prop1, prop2) -> ((eval prop1) && (eval (Not prop2))) || ((eval (Not prop1)) && (eval prop2))
+               | And3 (prop1, prop2, prop3) -> (eval prop1) && (eval prop2) && (eval prop3)     
 
 
 let shouldBeFalse = And (Not True, Not True)
@@ -24,12 +28,18 @@ let complexLogic = And (And(True, Or (Not(True), True)), Or (And (True, Not(True
 let XorTrue = Xor (True, Not True);
 let XorFalse = Xor (True, True);
 
+let And3True = And3 (True, True, True);
+let And3False = And3 (True, False, True);
+
 printfn "shouldBeFalse : %b" (eval shouldBeFalse)
 printfn "shouldBeTrue : %b" (eval shouldBeTrue)
 printfn "complexLogic : %b" (eval complexLogic)
 
-printfn "shouldBeFalse : %b" (eval XorFalse)
-printfn "shouldBeTrue : %b" (eval XorTrue)
+printfn "XOR shouldBeFalse : %b" (eval XorFalse)
+printfn "XOR shouldBeTrue : %b" (eval XorTrue)
+
+printfn "AND3 shouldBeFalse : %b" (eval And3False)
+printfn "AND3 shouldBeTrue : %b" (eval And3True)
 
 
 let main argv = 
