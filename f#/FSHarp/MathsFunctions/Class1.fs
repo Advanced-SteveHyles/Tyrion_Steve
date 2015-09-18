8191348fa49ac89c301fec66c6ac2c6285f22648
Steven Hyles <steven.hyles@advancedcomputersoftware.com>
2015-09-18 09:25:56 +0100

    Added Xunit tests and recursion examples

--- f#/FSHarp/MathsFunctions/Class1.fs
+++ f#/FSHarp/MathsFunctions/Class1.fs
@@ -1,7 +1,22 @@
 ﻿namespace MathsFunctions
 
-type Class1() = 
-   let rec factorial = function
-            | 0 -> 1
-            | 1 -> 1
-            | n -> n * factorial (n - 1)
+type MathClass() = 
+    
+    let rec factorial = function            
+            | 0  -> 1
+            | 1  -> 1
+            | n when n < 0 -> failwith "value cannot be < 0"
+            | n  -> n * factorial (n - 1)
+
+    let rec factorialf = function            
+            | 0.00  -> 1.00
+            | 1.00  -> 1.00
+            | n when n < 0.00 -> failwith "value cannot be < 0"
+            | n  -> n * factorialf (n - 1.00)
+
+    member this.GetFactorial x =
+        factorial x
+
+    member this.GetFactorialF x =
+        factorialf x
+
--- f#/FSHarp/UnitTests/Library1.fs
+++ f#/FSHarp/UnitTests/Library1.fs
@@ -1,10 +1,41 @@
-﻿namespace  UnitTests
+﻿module Tests
 
-open Xunit
-open MathsFunctions
+    open Xunit
+    open MathsFunctions
 
-[<Fact>]
-let ApplyingTheFactorialFunctionToTheValue5Yields120() = 
-    let result =  Class1.Factorial 5
+    [<Fact>]
+    let SimpleTest() =
 
-    Assert.Equal (120, fst(result))
+        let s1 = "stuff"
+
+        Assert.Equal<string>("stuff", s1)
+
+    [<Fact>]
+    let SimpleTest2() =
+
+        let s1 = "stuff"
+
+        Assert.Equal<string>("stuff", s1)
+        
+       
+    [<Fact>]
+    let ApplyingTheFactorialFunctionToTheValue5Yields120() = 
+                
+        let c = new MathClass()
+       
+        Assert.Equal<int> (120, c.GetFactorial 5)
+
+    [<Fact>]
+    let ApplyingTheFactorialFunctionToTheValueNegative5DoesSomething() = 
+                
+        let c = new MathClass()
+       
+        Assert.Equal<int> (120, c.GetFactorial -5)
+
+
+    [<Fact>]
+    let ApplyingTheFactorialFunctionToTheValue5p00Yields120() = 
+                
+        let c = new MathClass()
+       
+        Assert.Equal<float> (120.00, c.GetFactorialF 5.00)
