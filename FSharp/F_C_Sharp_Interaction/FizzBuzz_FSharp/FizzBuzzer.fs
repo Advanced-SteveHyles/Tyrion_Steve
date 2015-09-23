3dfe692232ec3547cfb5cbd64af31c530cc876cd
Steven Hyles <steven.hyles@advancedcomputersoftware.com>
2015-09-23 09:30:25 +0100

    Added C# and F# projects and cross call from one to the other.  An example KATA for this is used, FizzBuzz

---
+++ FSharp/F_C_Sharp_Interaction/FizzBuzz_FSharp/FizzBuzzer.fs
@@ -0,0 +1,23 @@
+﻿namespace FizzBuzz_FSharp
+
+type FixBuzzController() = 
+    
+    let Resolver1 x = 
+        if (x % 3) = 0 && (x % 5) = 0 then "FizzBuzz"
+        else
+            if (x % 3) = 0 then "Fizz"
+            else
+                if (x % 5) =0 then "Buzz"
+                else 
+                    let value = x.ToString()
+                    if value.Contains "3" then "Fizz"
+                    else
+                        if value.Contains "5" then "Buzz"
+                        else value                            
+               
+    let Resolver2 x = 
+        "Not implemented"
+                
+    member this.PlayFizBuzz x = 
+        Resolver2 x
+
---
+++ FSharp/F_C_Sharp_Interaction/Tests_CSharp/FizzBuzzTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using FizzBuzz_FSharp;
+
+namespace Tests_CSharp
+{
+    //http://codingdojo.org/cgi-bin/index.pl?KataFizzBuzz
+
+    public class FizzBuzzTests
+    {
+        private readonly FixBuzzController _fizzBuzzController;
+
+        public FizzBuzzTests()
+        {
+            _fizzBuzzController = new FixBuzzController();
+        }
+
+        [Fact]
+        public void ZeroReturnsFizzBuzz()
+        {
+            Assert.Equal("FizzBuzz",  _fizzBuzzController.PlayFizBuzz(0));
+            //Assert.Equal("FizzBuzz",  _fizzBuzzController.PlayFizBuzz(0));
+        }
+
+
+        [Fact]
+        public void OneReturnsOne()
+        {
+            Assert.Equal("1", _fizzBuzzController.PlayFizBuzz(1)); ;
+        }
+
+        [Fact]
+        public void TwoReturnsTwo()
+        {
+            Assert.Equal("2", _fizzBuzzController.PlayFizBuzz(2)); ;
+        }
+
+        [Fact]
+        public void ThreeReturnsFizz()
+        {
+            var actual = (_fizzBuzzController.PlayFizBuzz(3)); ;
+            Assert.Equal("Fizz", actual);
+        }
+
+        [Fact]
+        public void ThirteenReturnsFizz()
+        {
+            var actual = (_fizzBuzzController.PlayFizBuzz(13)); ;
+            Assert.Equal("Fizz", actual);
+        }
+
+        [Fact]
+        public void FiftyFiveReturnsBuzz()
+        {
+            var actual = (_fizzBuzzController.PlayFizBuzz(55)); ;
+            Assert.Equal("Buzz", actual);
+        }
+    }
+}
