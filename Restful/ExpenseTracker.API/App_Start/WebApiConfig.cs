09355bf3a6bc30a1eb43cd5b5a3ce2fccb7c2423
Steven Hyles <steven.hyles@advancedcomputersoftware.com>
2015-11-05 09:19:10 +0000

    Added support for Plura Rest Course

---
+++ Restful/ExpenseTracker.API/App_Start/WebApiConfig.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Web.Http;
+
+namespace ExpenseTracker.API
+{
+    public static class WebApiConfig
+    {
+        public static HttpConfiguration Register()
+        {
+            var config = new HttpConfiguration();
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+               name: "DefaultRouting",
+               routeTemplate: "api/{controller}/{id}",
+               defaults: new { id = RouteParameter.Optional }
+           );
+
+            //Ny Default web API support both JSON and XML.
+        //    ForceDefaultToJson(config);
+            TurnOffSupportForXml(config);
+             
+            return config;
+             
+        }
+
+        private static void TurnOffSupportForXml(HttpConfiguration config)
+        {
+            config.Formatters.XmlFormatter.SupportedMediaTypes.Clear();                 
+        }
+
+        private static void ForceDefaultToJson(HttpConfiguration config)
+        {
+            config.Formatters.JsonFormatter.SupportedMediaTypes
+                .Add(new MediaTypeHeaderValue("text/html"));
+        }
+    }
+}
---
+++ Restful/ExpenseTracker.API/Controllers/ExpenseGroupsController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ExpenseTracker.Repository;
+using ExpenseTracker.Repository.Entities;
+using ExpenseTracker.Repository.Factories;
+
+namespace ExpenseTracker.API.Controllers
+{
+    public class ExpenseGroupsController : ApiController
+    {                
+        private readonly IExpenseTrackerRepository _repository;
+        private readonly ExpenseGroupFactory _expenseGroupFactory = new ExpenseGroupFactory();
+        
+
+        public ExpenseGroupsController()
+        {
+            _repository = new ExpenseTrackerEFRepository(new ExpenseTrackerContext ());
+        }
+
+        public ExpenseGroupsController(IExpenseTrackerRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public IHttpActionResult Get()
+        {
+            try
+            {
+                var expenseGroups = _repository.GetExpenseGroups();
+
+                 return Ok(MapEntitiesToDtoModels(expenseGroups));
+            }
+            catch (Exception)
+            {
+                return InternalServerError();
+            }
+            
+        }
+
+        private IEnumerable<DTO.ExpenseGroup> MapEntitiesToDtoModels(IEnumerable<ExpenseGroup> expenseGroups)
+        {
+            return expenseGroups.ToList().Select(eg => _expenseGroupFactory.CreateExpenseGroup(eg));
+        }
+    }                                                       
+}
