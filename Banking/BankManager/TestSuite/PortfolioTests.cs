0085cf3a9f39ee65a6ceea565dbc374c601d16d8
Steven Hyles <steven.hyles@advancedcomputersoftware.com>
2016-01-14 13:05:11 +0000

    Suffering from Tech stack overload

--- Banking/BankManager/TestSuite/PortfolioTests.cs
+++ Banking/BankManager/TestSuite/PortfolioTests.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using Data.Accounts;
 using Xunit;
 
 namespace Data.Tests
@@ -7,17 +9,36 @@ namespace Data.Tests
     public class PortfolioTests
     {
         //Ultimate goal - read Portfolio data from Database
-       [Fact(DisplayName = "DB:BasicRead", Skip = "DatabaseBruteTest")]
-       [Trait("Database", "Brute")]
-       //[Fact(DisplayName = "DB:BasicRead")]
-        public   void ReadPortfolio ()
+        [Fact(DisplayName = "DB:BasicRead", Skip = "DatabaseBruteTest")]
+        [Trait("Database", "Brute")]
+        //[Fact(DisplayName = "DB:BasicRead")]
+        public void ReadPortfolio()
         {
             var x = new PortfolioManagerContext();
 
             var qry = x.Portfolios.Select(p => p.PortfolioName);
 
             Assert.Equal(qry.Count(), 3);
-           Assert.NotEmpty(qry.ToList());
+            Assert.NotEmpty(qry.ToList());
+        }
+
+        [Fact(DisplayName = "DB:PortfolioCreate")]
+        [Trait("Database", "Brute")]
+        //[Fact(DisplayName = "DB:BasicRead")]
+        public void CreatePortfolio()
+        {
+            var x = new PortfolioManagerContext();
+
+            var portfolio = new Portfolio();
+            var portfolioName = "Steves";
+            portfolio.PortfolioName = portfolioName;
+
+            x.Portfolios.Add(portfolio);
+
+            var qry = x.Portfolios.Select(p => p.PortfolioName);
+            Assert.Equal(1, qry.Count());
+            Assert.NotEmpty(qry.ToList());
+            Assert.Equal(portfolioName, qry.First());
         }
     }
 }
