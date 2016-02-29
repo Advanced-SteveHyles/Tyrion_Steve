ad9e7bf2d1f04fbb220e0f5018c849623ca69dbd
Steven Hyles <steven.hyles@advancedcomputersoftware.com>
2016-02-29 13:04:43 +0000

    More refactoring

---
+++ PortfolioManager2/Services/VirtualService/VirtualAPI.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PortfolioManager.DTO.Requests.Transactions;
+
+namespace VirtualService
+{
+    public class VirtualAPI
+    {
+
+        public IHttpActionResult Post([FromBody] PriceHistoryRequest request)
+        {
+            
+        }
+    }
+}
---
+++ PortfolioManager2/Services/VirtualService/VirtualControllers/Class1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace VirtualService.VirtualControllers
+{
+    public class PriceUpdateController
+    {
+        private readonly AccountRepository _accountRepository;
+        private readonly PriceHistoryRepository _priceHistoryRepository;
+        private AccountInvestmentMapRepository _accountInvestmentMapRepository;
+
+        public PriceUpdateController()
+        {
+            var context = new PortfolioManagerContext();
+            _accountInvestmentMapRepository = new AccountInvestmentMapRepository(context);
+            _priceHistoryRepository = new PriceHistoryRepository(context);
+            _accountRepository = new AccountRepository(context);
+        }
+
+        [System.Web.Http.HttpPost]
+        [Route(ApiPaths.InvestmentSinglePriceUpdate)]
+        public IHttpActionResult Post([FromBody] PriceHistoryRequest request)
+        {
+
+            try
+            {
+                if (request == null)
+                {
+                    return BadRequest();
+                }
+
+                var entityPriceHistory = new PriceHistoryFactory().CreatePriceHistory(request);
+                if (entityPriceHistory == null)
+                {
+                    return BadRequest();
+                }
+
+                /*
+                {
+                    "userId": "https://expensetrackeridsrv3/embedded_1",
+                    "title": "STV",
+                    "description": "STV",
+                    "expenseGroupStatusId": 1,
+                }
+                */
+                var historyHandler = new PriceHistoryHandler(_priceHistoryRepository);
+                var priceHistoryProcessor = new RecordPriceHistoryProcessor(request, historyHandler);
+                var revalueSinglePriceCommand = new RevalueSinglePriceCommand(
+                    request.InvestmentId,
+                    request.ValuationDate,
+                    historyHandler,
+                    new AccountInvestmentMapProcessor(_accountInvestmentMapRepository),
+                    new AccountHandler(_accountRepository)
+                    );
+
+                priceHistoryProcessor.Execute();
+                revalueSinglePriceCommand.Execute();
+
+                if (priceHistoryProcessor.ExecuteResult && revalueSinglePriceCommand.ExecuteResult)
+                {
+                    //var dtoPortfolio = result.Entity.MapToDto();
+                    return Created(Request.RequestUri + "/", new { });
+                }
+                else
+                {
+                    return BadRequest();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                ErrorLog.LogError(ex);
+                return InternalServerError();
+            }
+        }
+    }
+}
