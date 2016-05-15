23df6e80e43f9ed8fefedda88d90aab9a428c421
STV <Steven.Hyles@hotmail.co.uk>
2016-05-15 11:53:42 +0100

    Reworking of node setup

---
+++ ReactSteve/up.bat
@@ -0,0 +1,8 @@
+call npm install
+call npm run build:dev
+call npm run build:favicon
+call npm run build:indexhtml
+call npm run build:bootstrap
+start chrome build/index.html
+call npm run build:watch
+
