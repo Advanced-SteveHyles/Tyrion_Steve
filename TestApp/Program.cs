1e88927e1c410d7ba166890cf84803dde52b899c
Steven Hyles <steven.hyles@advancedcomputersoftware.com>
2016-06-29 09:06:49 +0100

    Added basic Self-Host application

---
+++ TestApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = new ServiceReference1.HelloWorldServiceClient();
+
+            var reply = x.About();
+            Console.WriteLine(reply);
+
+            var name = "Steve";
+            reply = x.SayHello(name);
+            Console.WriteLine(reply);
+        }
+    }
+}
